a163f3ab56e9f7ff05c0a765d91e1063677b94a7
Pavel Hubarenka <pavel-se47@mail.ru>
2022-12-15 14:46:30 +0300

    add timer

--- src/index.js
+++ src/index.js
@@ -1,33 +1,55 @@
-const colors = [
-  '#FFFFFF',
-  '#2196F3',
-  '#4CAF50',
-  '#FF9800',
-  '#009688',
-  '#795548',
-];
-const changeColorEl = document.querySelector('#change-color');
-const startBtnEl = document.querySelector('button[data-action="start"]');
-const stopBtnEl = document.querySelector('button[data-action="stop"]');
+const bodyEl = document.querySelector('#main-timer');
+const daysEl = document.querySelector('[data-value="days"]');
+const hoursEl = document.querySelector('[data-value="hours"]');
+const minsEl = document.querySelector('[data-value="mins"]');
+const secsEl = document.querySelector('[data-value="secs"]');
 
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+class CountdownTimer {
+  constructor({ onTick, selector, targetDate }) {
+    this.onTick = onTick;
+    this.selector = selector;
+    this.targetDate = targetDate;
+  }
+
+  remainingTime() {
+    bodyEl.firstElementChild.setAttribute('id', this.selector);
+    setInterval(() => {
+      const currentDate = Date.now();
+      const deltaTime = this.targetDate - currentDate;
+      const time = getTimeComponents(deltaTime);
+      this.onTick(time);
+    }, 1000);
+  }
+}
+
+const getTimeComponents = time => {
+  const days = Math.floor(time / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+  return { days, hours, mins, secs };
 };
 
-let timerChangeColor = null;
-
-const startChangeColor = () => {
-  startBtnEl.setAttribute('disabled', true);
-  timerChangeColor = setInterval(() => {
-    changeColorEl.style.backgroundColor =
-      colors[randomIntegerFromInterval(0, colors.length)];
-  }, 1000);
+const updateClockFace = ({ days, hours, mins, secs }) => {
+  daysEl.textContent = `${days}`;
+  hoursEl.textContent = `${hours}`;
+  minsEl.textContent = `${mins}`;
+  secsEl.textContent = `${secs}`;
 };
 
-const stopChangeColor = () => {
-  startBtnEl.removeAttribute('disabled');
-  clearInterval(timerChangeColor);
-};
+const timerOne = new CountdownTimer({
+  onTick: updateClockFace,
+  selector: '#timer-1',
+  targetDate: new Date('Jul 17, 2023'),
+});
 
-startBtnEl.addEventListener('click', startChangeColor);
-stopBtnEl.addEventListener('click', stopChangeColor);
+timerOne.remainingTime();
+
+// const timerTwo = new CountdownTimer({
+//   onTick: updateClockFace,
+//   selector: '#timer-2',
+//   targetDate: new Date('Jul 25, 2024'),
+// });
+
+// timerTwo.remainingTime();
